7340e4abd8e25aeea86a3d5b381a3d444e5bf215
Akanate458 <nathanbenmarsden@hotmail.com>
2022-03-06 13:11:04 +0000

    Had conflicts with css class names fixed them

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,8 +2,8 @@ import "../styles/container.css"
 
 export default function Container (props) {
     return (
-        <div className="Header--container">
-            <div className="Header" style={props.applyStyles}></div>
+        <div className="HeaderContainer--container">
+            <div className="HeaderContainer" style={props.applyStyles}></div>
         </div>
     )
 }
