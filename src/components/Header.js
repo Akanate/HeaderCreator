785ae506c58da0d87a6748bf30e71138b0a72b3c
Akanate458 <nathanbenmarsden@hotmail.com>
2022-03-06 12:08:04 +0000

    Changed text in header to "Header Creator"

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import "../styles/header.css"
 export default function Header () {
     return (
         <header className="Header">
-            <p className="Header--text-border">Border Creator</p>
+            <p className="Header--text-border">Header Creator</p>
             <p className="Header--text-user">By Akanate</p>
         </header>
     )
